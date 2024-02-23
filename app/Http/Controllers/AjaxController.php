4ce730536e34bc678b734aa2405ff4acab253278
alexiskatel.contact <alexiskatel.contact@gmail.com>
2024-02-23 09:23:21 +0100

    CRUD Enseignant

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Banque;
+use App\Models\Enseignant;
 use Illuminate\Http\Request;
 
 class AjaxController extends Controller
@@ -14,5 +15,12 @@ class AjaxController extends Controller
         notyf()->addSuccess('Banque supprimée avec success.');
         return response()->json(['success' => true]);
     }
-    
+
+    public function deleteEnseignants(Request $request)
+    {
+        Enseignant::whereIn('id', $request->enseignants_ids)->delete();
+        notyf()->addSuccess('Enseignant supprimé avec succès.');
+        return response()->json(['success' => true]);
+    }
+
 }
