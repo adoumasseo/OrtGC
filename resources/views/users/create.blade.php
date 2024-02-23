bba4c8f33f46d798a27325de088130588f647595
Carlos-H-24 <akodjenouherve13@gmail.com>
2024-02-23 13:34:14 +0100

    Contrat edit

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -6,9 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" type="text/css" />
 @endsection
 @section('content')
-
     <div class="border card rounded-0">
-
         <div class="card-body">
             <form action="{{ route('cycles.store') }}" method="post">
                 @csrf
